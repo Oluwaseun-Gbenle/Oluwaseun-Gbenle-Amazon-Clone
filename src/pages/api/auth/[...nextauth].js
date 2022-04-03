1c41b4d3384d64dfb314119040b4b584bdc5d240
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2022-04-03 13:40:27 +0100

    new file

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -10,6 +10,13 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
    /* EmailProvider({
       server: process.env.MAIL_SERVER,
