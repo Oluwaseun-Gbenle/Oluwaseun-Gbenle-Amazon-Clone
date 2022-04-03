49c0cd1e5e2162128fcb108eb6f07df9be9b68fd
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2022-04-03 14:15:42 +0100

    new

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,12 +5,19 @@ import EmailProvider from "next-auth/providers/email"
 
 
 export default NextAuth({
+  secret: process.env.SECRET,
   providers: [
     // OAuth authentication providers
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
    /* EmailProvider({
       server: process.env.MAIL_SERVER,
@@ -18,22 +25,5 @@ export default NextAuth({
     }),*/
     // Sign in with passwordless email link
   ],
-  jwt: {
-    encryption: true
-  },
-  secret: process.env.SECRET,
-  callbacks: {
-    async jwt(token, account) {
-      if (account ?.accessToken) {
-        token.accessToken = account.accessToken
-      }
-      return token;
-    },
-    redirect: async (url, _baseUrl)=>{
-      if (url === '/user') {
-        return Promise.resolve('/')
-      }
-      return  Promise.resolve('/')
-    }
-}
+
 })
